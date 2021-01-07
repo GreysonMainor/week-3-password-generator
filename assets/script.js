9983942aff5f0e11701a237e452316eef37cc984
Greyson Mainor <gbmainor98@outlook.com>
2021-01-07 18:13:36 -0500

    added concatination to passgen as wellas fixing my character count alert.

--- assets/script.js
+++ assets/script.js
@@ -7,54 +7,62 @@ function writePassword(makeItWork) {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-var lowercaseChar = ("abcdefghijklmnopqrstuvwxyz")
-var uppercaseChar = ("ABCDEFGHIJKLMNOPQRSTUVWXTZ")
-var specialChar = ("~!%^&*<>")
-var numbersChar = ("1234567890")
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz"
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXTZ"
+var specialChar = "~!%^&*<>"
+var numbersChar = "1234567890"
 var randomPassword;
 var passGen = "";
 
 function generatePassword() {
   var passwordLength = prompt("Your Password must greater than 8 characters and no more than 128. Please choose your character number.");
-  if (passwordLength > 8, passwordLength > 128) {
+  if (passwordLength < 8 || passwordLength > 128) {
     alert("Password does not fit parameters given. please revise.");
     return;
   }
-  console.log(passwordLength)
 
-  var uppercase = confirm("Do you want uppercase letters in your password?")
+  var uppercase = confirm("Do you want uppercase letters in your password?");
   if (uppercase) {
-    alert("adding uppercase letters to the password.")
+    alert("adding uppercase letters to the password.");
+    passGen += uppercaseChar
   } else {
-    alert("No uppercase letters will be added.")
+    alert("No uppercase letters will be added.");
   }
 
-  var lowercase = confirm("Do you want lowercase letters in your password?")
+  var lowercase = confirm("Do you want lowercase letters in your password?");
   if (lowercase) {
-    alert("adding lowercase letters to your password.")
+    alert("adding lowercase letters to your password.");
+    passGen += lowercaseChar
   } else {
-    alert("No lowercase letters will be added.")
+    alert("No lowercase letters will be added.");
   }
 
-  var special = confirm("do you want special characters in your password?")
+  var special = confirm("do you want special characters in your password?");
   if (special) {
-    alert("adding special characters to your password.")
+    alert("adding special characters to your password.");
+    passGen += specialChar
   } else {
-    alert("No special characters will be added to your password.")
+    alert("No special characters will be added to your password.");
   }
 
-  var numbers = confirm("do you want numbers in your password?")
+  var numbers = confirm("do you want numbers in your password?");
   if (numbers) {
-    alert("adding numbers to your password.")
+    alert("adding numbers to your password.");
+    passGen += numbersChar
   } else {
-    alert("no numbers will be added to your password.")
+    alert("no numbers will be added to your password.");
   }
 
-  console.log(uppercase)
-  console.log(lowercase)
-  console.log(special)
-  console.log(numbers)
+  console.log(uppercase);
+  console.log(lowercase);
+  console.log(special);
+  console.log(numbers);
+  console.log(passGen);
+  console.log(passwordLength);
+
+
+
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword, generatePassword);
+generateBtn.addEventListener("click", generatePassword);
